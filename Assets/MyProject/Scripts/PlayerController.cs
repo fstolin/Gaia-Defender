7d3ae70b472e91dc526e341fbce92b1057b7f36f
gogomaester <stolin.f@gmail.com>
2022-11-17 21:36:29 +0100

    Finishing movement + limiting the movement

--- Assets/MyProject/Scripts/PlayerController.cs
+++ Assets/MyProject/Scripts/PlayerController.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    void Start()
-    {
-
-    }
+    [SerializeField] float movementSpeed = 30f;
+    [SerializeField] float movementMaximumOffsetX = 30f;
+    [SerializeField] float movementMaximumOffsetY = 12f;
+    [SerializeField] float movementMinimumOffsetY = 4f;
 
 
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        HandleMovement();
     }
 
+    private void HandleMovement()
+    {
+        // Get input
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+        // Calculate the offset from the original position this frame
+        float xOffset = (movementSpeed * xThrow * Time.deltaTime);
+        float yOffset = (movementSpeed * yThrow * Time.deltaTime);
+        // Calculate the new position this frame
+        float newXPosition = transform.localPosition.x + xOffset;
+        float newYPosition = transform.localPosition.y + yOffset;
+        // Limit the movement
+        newXPosition = Mathf.Clamp(newXPosition, -movementMaximumOffsetX, movementMaximumOffsetX);
+        newYPosition = Mathf.Clamp(newYPosition, movementMinimumOffsetY, movementMaximumOffsetY);
+        // Local position update
+        transform.localPosition = new Vector3
+               (newXPosition,
+               newYPosition,
+               transform.localPosition.z);
+    }
 
 }
